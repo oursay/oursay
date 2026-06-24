f216759055343c075869b8e3c83486f028ae0ed5
OurSay <oursay.ca@gmail.com>
2026-06-24 13:27:57 -0600

    add ab-ca-gov district shapefiles for 2019 + 2023

---
+++ jurisdiction-data/ab-ca-gov/jurisdiction.ts
@@ -0,0 +1,7 @@
+/* 
+To be used to simulate the jurisdiction config for the ab-ca-gov jurisdiction. 
+Contains rules such as censorship policies, who can write to the jurisdiction 
+chain, which entities need presence based signatures, if users can edit their 
+votes, comments, revoke petition signatures, and other rules that govern the 
+jurisdiction.
+*/
